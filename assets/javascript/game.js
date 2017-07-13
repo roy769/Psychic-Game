ee256fca739dc926501df2715aaae99097dc19c0
roy769 <roy@mortgallery.com>
2017-07-13 11:26:34 -0700

    fixed img issue?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,57 @@
 
-	var  computerChoices = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var  computerChoices = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-	var correct = 0;
-	var wrong = 0;
-	var guessesLeft = 9;
+var correct = 0;
+var wrong = 0;
+var guessesLeft = 9;
 	/*for(var i =0; i < guessesLeft.length; i--)*/
-	var guesses = 0;
-	var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+var guesses = 0;
 
 
-	document.onkeyup = function (event) {
+var  computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+
+
+	document.onkeyup = function(event) {
 
 
 
 		var userGuess = event.key;
 
 		console.log(computerGuess);
+		//put counter for guesses
+		//limit guesses to 9
+		//send t educated-guess page at guess 10
 
 
-
-	if (userGuess != computerGuess) { wrong++;
-		guessesLeft--; guesses++;
+	if (userGuess != computerGuess)
+	 { wrong++;
+		guessesLeft--; guesses++; event++;
 	}
-
 
 			else { correct++;
 				alert("The letter was " + computerGuess);
 				alert("	YOU ARE PSYCHIC, good job " );
 
 				}
-				if (guesses > 9)
-				{
-					window.location.href = "file:///C:/Users/dasik/git/Psychic-Game/educated-guess.html";
-				}
-			}
+if (guessesLeft === 0) {
+	confirm("Wanna ask Miss Cleo for help?! ");
+	location.target = "_blank";
+    location.href = "file:///C:/Users/dasik/git/Psychic-Game/educated-guess.html";
+	//link here
+}
+
+					//end to educated-guess page
+
 
 		var html =
 
 		"<p> You Chose:  "  + userGuess.toUpperCase()+ "</p>" +
 		"<p> Wrong: " + wrong + "</p>"+
 		"<p> Correct: " + correct + "</p>" +
-		"<p> Guesses Left: " + guessesLeft + "</p>";
+		"<p> Guesses Left: " + guessesLeft + "</p>" +
+		"<p> Guesses: " + guesses + "</p>"+
+		"<P> Guess History: " + event + "</p>";
 
 		document.querySelector(".psychic").innerHTML = html;
 	}
